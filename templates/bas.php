10f3b1feb77cbf0eac9157c9ded492c2f94df167
Florian <florian.janson@gmail.com>
2011-11-08 16:54:04 +0100

    Creation de index.php, haut.php & bas.php

---
+++ templates/bas.php
@@ -0,0 +1,7 @@
+ </div>
+        </div>
+        <div id="footer">
+            <img alt="fond bot" src="./templates/images/contenu_fond_bot.png"/>
+        </div>
+    </body>
+</html>
---
+++ templates/haut.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns ="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <head>
+        <title><?php echo $title; ?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" type="text/css" href="./templates/cssMdh.css" media="screen" />
+
+    </head>
+    <body>
+        <div id="header">
+            <div id="logo">
+                <a href="">
+                    <img alt="mdh" src="./templates/images/mdh2.png"/>
+                </a>
+            </div>
+            <div id="navigation">
+                <ul>
+                    <li id="accueil_menu" class="currentpage"><a href="" title="">Accueil<span class="hover"></span></a></li>
+                    <li id="agence_menu" class=""><a href="" title="">Qui sommes-nous<span class="hover"></span></a></li>
+                    <li id="services_menu" class=""><a href="" title="">Savoir faire<span class="hover"></span></a></li>
+                    <li id="realisations_menu" class=""><a href="" title="">Réalisations<span class="hover"></span></a></li>
+                    <li id="contact_menu" class=""><a href="" title="">Contact<span class="hover"></span></a></li>
+                </ul>
+            </div>
+            <div id="top">
+                <img alt="fond top" src="./templates/images/contenu_fond_top.png"/>
+            </div>
+        </div>
+        <div id="content_background">
+            <div id="content">
