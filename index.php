cab56757ff57e249d4658ebe7e8bbb8acdd40aa0
PyroXn <pierre.charrasse@gmail.com>
2011-11-16 00:12:24 +0100

    probleme moz

--- index.php
+++ index.php
@@ -109,60 +109,61 @@ function services() {
     $contenu .= '
         <li>
             <a href="#" title="Site web">
-                <h3>Site web</h3>
-                <p>Pour vous, notre équipe est capable de concevoir des sites internet allant du site vitrine au site e-commerce en passant par 
-                    des sites événementiels, communautaires, des blogs ou toutes autres demandes spécifiques.
-                    <em>En savoir</em>
-                </p>
+                <span class="titre_liste">Site web</span>
+                    <span class="paragraphe_liste">Pour vous, notre équipe est capable de concevoir des sites internet allant du site vitrine au site e commerce en passant par 
+                        des sites événementiels, communautaires, des blogs ou toutes autres demandes spécifiques.
+                        <em>En savoir</em>
+                    </span>
+                </span>
             </a>
         </li>';
     $contenu .= '
         <li>
             <a href="#" title="Logiciel">
-                <h3>Logiciel</h3>
-                <p>Vous souhaitez informatiser, optimiser ou disposer de logiciel efficace pour votre entreprise? Notre équipe spécialisé dans le
+                <span class="titre_liste">Logiciel</span>
+                <span class="paragraphe_liste">Vous souhaitez informatiser, optimiser ou disposer de logiciel efficace pour votre entreprise? Notre équipe spécialisé dans le
                     développement de progiciel est à votre écoute.
                     <em>En savoir</em>
-                </p>
+                </span>
             </a>
         </li>';
 
     $contenu .= '
         <li>
             <a href="#" title="Design">
-                <h3>Design</h3>
-                <p> Pour votre identité visuelle nous créons ou relookons vos chartes graphiques, logos ainsi que différents supports tels que des 
+                <span class="titre_liste">Design</span>
+                <span class="paragraphe_liste">Pour votre identité visuelle nous créons ou relookons vos chartes graphiques, logos ainsi que différents supports tels que des 
                     cartes de visite, plaquettes commerciales, flyers, newsletters…
                     <em>En savoir</em>
-                </p>
+                </span>
             </a>
         </li>';
     
     $contenu .= '
         <li>
             <a href="#" title="Referencement">
-                <h3>Referencement</h3>
-                <p>Vous souhaitez améliorer le positionnement et la visibilitée de votre site internet sur les moteurs de recherche ? 
+                <span class="titre_liste">Referencement</span>
+                <span class="paragraphe_liste">Vous souhaitez améliorer le positionnement et la visibilitée de votre site internet sur les moteurs de recherche ? 
                     Nous proposons diverses offres vous permettant d\'acceder à un référencement durable. 
                     <em>En savoir</em>
-                </p>
+                </span>
             </a>
         </li>';
     
     $contenu .= '
         <li>
-            <h3>Formation</h3>
-            <p>My Dev House vous propose des formations sur mesure. Nous établissons ensemble les points sur lesquels vous souhaitez être
+            <span class="titre_liste">Formation</span>
+            <span class="paragraphe_liste">My Dev House vous propose des formations sur mesure. Nous établissons ensemble les points sur lesquels vous souhaitez être
             formé ce qui vous assure une formation utile et constructive.
-            </p>
+            </span>
         </li>';
     
     $contenu .= '
         <li>
-            <h3>Application smartphone</h3>
-            <p>Nous proposons à nos clients des applications smartphone sur mesure. Nous sommes à l\'écoute de vos besoins afin de vous 
+            <span class="titre_liste">Application smartphone</span>
+            <span class="paragraphe_liste">Nous proposons à nos clients des applications smartphone sur mesure. Nous sommes à l\'écoute de vos besoins afin de vous 
             proposez une solution au plus près de vos attentes.
-            </p>
+            </span>
         </li>';
 
     $contenu .= '</ul>';
