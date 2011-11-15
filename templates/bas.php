9179fb4b5017e2ee1384d13868c9660d0cda3374
PyroXn <pierre.charrasse@gmail.com>
2011-11-15 13:51:27 +0100

    premice footer

--- templates/bas.php
+++ templates/bas.php
@@ -1,8 +1,38 @@
  </div>
  <div class="clearboth"></div>
         </div>
-        <div id="footer">
+        <div id="bot">
             <img alt="fond bot" src="./templates/images/contenu_fond_bot.png"/>
+        </div>
+        <div id="footer">
+            <div id="wrapper">
+                <div id="footer_top">
+                    <div class="col-left"><img src="./templates/images/mdh2.png" /></div>
+                    <div class="col-right">
+                        <ul>
+                            <li><a href="http://facebook.com/">Facebook</a></li>
+                            <li><a href="http://twitter.com/">Twitter</a></li>
+                            <li><a href="http://youtube.com/">Youtube</a></li>
+                        </ul>
+                    </div>
+                    <div class="clearboth"></div>
+                </div>
+                <div id="footer_mid">
+                    
+                    <div class="clearboth"></div>
+                </div>
+                <div id="footer_bottom">
+                    <ul>
+                        <li id="accueil_menu" class="currentpage"><a href="index.php?p=home" title="">Accueil<span class="hover"></span></a></li>
+                        <li id="agence_menu" class=""><a href="index.php?p=agence" title="">Qui sommes-nous<span class="hover"></span></a></li>
+                        <li id="services_menu" class=""><a href="index.php?p=services" title="">Savoir faire<span class="hover"></span></a></li>
+                        <li id="realisations_menu" class=""><a href="index.php?p=realisations" title="">Réalisations<span class="hover"></span></a></li>
+                        <li id="contact_menu" class=""><a href="index.php?p=contact" title="">Contact<span class="hover"></span></a></li>
+                    </ul>
+                    <div class="clearboth"></div>
+                </div>
+                <div class="copyrigth">&copy; 2011 My Dev House Sarl</div>
+            </div>
         </div>
     </body>
 </html>
