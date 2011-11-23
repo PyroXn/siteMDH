fb6737139e8cd9b0e9e082b5618d59e34cb0d9b0
PyroXn <pierre.charrasse@gmail.com>
2011-11-23 15:01:35 +0100

    footer social

--- templates/bas.php
+++ templates/bas.php
@@ -10,22 +10,18 @@
             </div>
             <div id="wrapper">
                 
-                <div id="footer_top">
-                    <div class="col-left"><img src="./templates/images/mdh2.png" /></div>
-                    <div class="col-right">
-                        <ul>
-                            <li><a href="http://facebook.com/">Facebook</a></li>
-                            <li><a href="http://twitter.com/">Twitter</a></li>
-                            <li><a href="http://youtube.com/">Youtube</a></li>
-                        </ul>
+                <div id="footer_content">
+                    <ul id="social">
+                            <li id="facebook"><a href="http://facebook.com/"><span class="image_hover"></span></a></li>
+                            <li id="google"><a href="https://plus.google.com/"><span class="image_hover"></span></a></li>
+                            <li id="github"><a href="https://github.com/"><span class="image_hover"></span></a></li>
+                    </ul>
+                    <div class="col-left">
+                         <!--<img src="./templates/images/mdh_small.png" />-->
                     </div>
-                    <div class="clearboth"></div>
-                </div>
-                <div id="footer_mid">
-                    
-                    <div class="clearboth"></div>
-                </div>
-                <div id="footer_bottom">
+                    <div class="col-right">
+                        
+                    </div>
                     <ul>
                         <li id="accueil_menu" class="currentpage"><a href="index.php?p=home" title="">Accueil<span class="hover"></span></a></li>
                         <li id="agence_menu" class=""><a href="index.php?p=agence" title="">Qui sommes-nous<span class="hover"></span></a></li>
@@ -35,7 +31,7 @@
                     </ul>
                     <div class="clearboth"></div>
                 </div>
-                <div class="copyrigth">&copy; 2011 My Dev House Sarl</div>
+                <div class="copyright">&copy; 2011 My Dev House Sarl</div>
             </div>
             <div class="bot">
                 <img alt="fond bot" src="./templates/images/footer_fond_bot.png"/>
