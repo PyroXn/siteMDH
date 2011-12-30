3302310958e5fd7aa3baaccde8fefe70c22184c0
PyrO <pierre.charrasse@gmail.com>
2011-12-30 17:14:33 +0100

    footer

--- templates/bas.php
+++ templates/bas.php
@@ -1,38 +1,40 @@
  </div>
  <div class="clearboth"></div>
-        </div>
+        <div id="footer_fond">
         <div class="bot">
             <img alt="fond bot" src="./templates/images/contenu_fond_bot.png"/>
         </div>
-        <div id="footer">
-            <div class="top">
-                <img alt="fond top" src="./templates/images/footer_fond_top.png"/>
-            </div>
-            <div id="wrapper">
-                
-                <div id="footer_content">
-                    <ul id="social">
-                            <li id="facebook"><a href="http://facebook.com/"><span class="image_hover"></span></a></li>
-                            <li id="google"><a href="https://plus.google.com/"><span class="image_hover"></span></a></li>
-                            <li id="github"><a href="https://github.com/"><span class="image_hover"></span></a></li>
-                    </ul>
-                    <div class="col-left">
-                         <!--<img src="./templates/images/mdh_small.png" />-->
-                    </div>
-                    <div class="col-right">
-                        
-                    </div>
-                    <ul>
-                        <li id="accueil_menu" class="currentpage"><a href="index.php?p=home" title="">Accueil<span class="hover"></span></a></li>
-                        <li id="agence_menu" class=""><a href="index.php?p=agence" title="">Qui sommes-nous<span class="hover"></span></a></li>
-                        <li id="services_menu" class=""><a href="index.php?p=services" title="">Savoir faire<span class="hover"></span></a></li>
-                        <li id="realisations_menu" class=""><a href="index.php?p=realisations" title="">Réalisations<span class="hover"></span></a></li>
-                        <li id="contact_menu" class=""><a href="index.php?p=contact" title="">Contact<span class="hover"></span></a></li>
-                    </ul>
-                    <div class="clearboth"></div>
+        
+            <div id="footer">
+                <div class="top">
+                    <img alt="fond top" src="./templates/images/footer_fond_top.png"/>
                 </div>
-                <div class="copyright">&copy; 2011 My Dev House - <a href="index.php?p=mentions">Mentions légales</a></div>
+                <div id="wrapper">
+                    <div id="footer_content">
+                        <ul id="social">
+                                <li id="facebook"><a href="http://facebook.com/"><span class="image_hover"></span></a></li>
+                                <li id="google"><a href="https://plus.google.com/"><span class="image_hover"></span></a></li>
+                                <li id="github"><a href="https://github.com/"><span class="image_hover"></span></a></li>
+                        </ul>
+                        <div class="col-left">
+                             <!--<img src="./templates/images/mdh_small.png" />-->
+                        </div>
+                        <div class="col-right">
+
+                        </div>
+                        <ul>
+                            <li id="accueil_menu" class="currentpage"><a href="index.php?p=home" title="">Accueil<span class="hover"></span></a></li>
+                            <li id="agence_menu" class=""><a href="index.php?p=agence" title="">Qui sommes-nous<span class="hover"></span></a></li>
+                            <li id="services_menu" class=""><a href="index.php?p=services" title="">Savoir faire<span class="hover"></span></a></li>
+                            <li id="realisations_menu" class=""><a href="index.php?p=realisations" title="">Réalisations<span class="hover"></span></a></li>
+                            <li id="contact_menu" class=""><a href="index.php?p=contact" title="">Contact<span class="hover"></span></a></li>
+                        </ul>
+                        <div class="clearboth"></div>
+                        <div class="copyright">&copy; 2011 My Dev House - <a href="index.php?p=mentions">Mentions légales</a></div>
+                     </div>
+                </div>
             </div>
+
             <div class="bot">
                 <img alt="fond bot" src="./templates/images/footer_fond_bot.png"/>
             </div>
