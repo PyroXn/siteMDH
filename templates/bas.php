b1293dacb2803294854fdb4fee6a55cd96019a5d
PyrO <pierre.charrasse@gmail.com>
2012-01-07 16:58:05 +0100

    footer

--- templates/bas.php
+++ templates/bas.php
@@ -11,24 +11,27 @@
                 </div>
                 <div id="wrapper">
                     <div id="footer_content">
-                        <ul id="social">
-                                <li id="facebook"><a href="http://facebook.com/"><span class="image_hover"></span></a></li>
-                                <li id="google"><a href="https://plus.google.com/"><span class="image_hover"></span></a></li>
-                                <li id="github"><a href="https://github.com/"><span class="image_hover"></span></a></li>
-                        </ul>
+                        
                         <div class="col-left">
                              <!--<img src="./templates/images/mdh_small.png" />-->
                         </div>
                         <div class="col-right">
 
                         </div>
-                        <ul>
-                            <li id="accueil_menu" class="currentpage"><a href="index.php?p=home" title="">Accueil<span class="hover"></span></a></li>
-                            <li id="agence_menu" class=""><a href="index.php?p=agence" title="">Qui sommes-nous<span class="hover"></span></a></li>
-                            <li id="services_menu" class=""><a href="index.php?p=services" title="">Savoir faire<span class="hover"></span></a></li>
-                            <li id="realisations_menu" class=""><a href="index.php?p=realisations" title="">Réalisations<span class="hover"></span></a></li>
-                            <li id="contact_menu" class=""><a href="index.php?p=contact" title="">Contact<span class="hover"></span></a></li>
+                        <ul id="lien_footer">
+                            <li id="accueil_menu"><a href="index.php?p=home" title="">Accueil</a></li>
+                            <li id="agence_menu" class=""><a href="index.php?p=agence" title="">Qui sommes-nous</a></li>
+                            <li id="services_menu" class=""><a href="index.php?p=services" title="">Savoir faire</a></li>
+                            <li id="realisations_menu" class=""><a href="index.php?p=realisations" title="">Réalisations</a></li>
+                            <li id="contact_menu" class=""><a href="index.php?p=contact" title="">Contact</a></li>
                         </ul>
+                        <ul id="social">
+                                <li id="facebook"><a href="http://facebook.com/"></a></li>
+                                <li id="google"><a href="https://plus.google.com/"></a></li>
+                                <li id="github"><a href="https://github.com/"></a></li>
+                        </ul>
+                        
+                        
                         <div class="clearboth"></div>
                         <div class="copyright">&copy; 2011 My Dev House - <a href="index.php?p=mentions">Mentions légales</a></div>
                      </div>
